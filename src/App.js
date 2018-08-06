cd4326dbe4ac601e31cd0a7a7e4527084eb93330
Piyush Mehta <me@piyushmehta.com>
2018-08-06 18:31:35 +0530

    Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 import 'typeface-roboto';
-import Typography from '@material-ui/core/Typography';
-const piyush = {
-  textAlign: 'center',
-  color: '#757575'
-};
+import Name from './components/Name';
+import Info from './components/Info';
 
 class App extends Component {
   render() {
@@ -14,12 +11,8 @@ class App extends Component {
       <div>
         <Grid container spacing={24}>
           <Grid item xs={12} sm={12} md={12} lg={12}>
-            <Typography variant="display3" style={piyush}>
-              Piyush Mehta
-            </Typography>
-            <Typography variant="subheading" style={piyush}>
-              Student || Web Developer || Future Data Scientist
-            </Typography>
+            <Name />
+            <Info />
           </Grid>
         </Grid>
       </div>
