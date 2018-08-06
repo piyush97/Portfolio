8233c93c20c446fe9c8ab0528db3d493f18c1132
Piyush Mehta <me@piyushmehta.com>
2018-08-06 19:38:46 +0530

    timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import './verticalTimeline.css';
+import './verticalTimelineElement.css';
 
 class Timeline extends Component
 {
   render(){
     return(
       <VerticalTimeline animate={true}>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2011 - present"
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
-      </VerticalTimelineElement>
-    </VerticalTimeline>
+        <VerticalTimelineElement className="vertical-timeline-element--work" date = "June 2018 - August 2018" iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} >
+          <h3 className="vertical-timeline-element-title">Software Development Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">Meaww, Blr, India</h4>
+          <div>
+            <p>Meaww(Media, Entertainment, Arts, WorldWide) is one of the Top 500 Websites in the world.</p>
+            <p>Made a full - fledged Dashboard with amazing GUI on ReactJs using charting libraries like D3JS and Chartist.Saved states using Redux. Worked on XHR, RESTful APIs and Javascript Data structures.</p>
+            <p>Technologies used: NodeJS, React.Js, Redux, PostgreSQL, MySQL, AWS, RabbitMQ, Hadoop, Spark.</p>
+            <p><a href='http://dev.inf.meaww.com'>See Project</a> </p>
+          </div>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className="vertical-timeline-element--work" date = "March 2018 - May 2018" iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }} >
+          <h3 className="vertical-timeline-element-title">Full Stack Developer Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">Aidapp, Blr, India</h4>
+          <div>
+            <p>Served as a key member of the executive team charged with Front - End Development for the upcoming Projects for the company.</p>
+            <p>Strategically managed Projects Especially the Front - End Work Built and managed a business development team to enhance and refine customer relationships and satisfaction.</p>
+            <p>Collaborated advertising company to rebrand website, marketing materials and sales strategy resulting in increased brand awareness and sales.</p>
+            <p>Made SaleDrive(Front - End) and Aidapp.in(Full Stack)</p>
+            <p><a href='http://aidapp.in'>See Website</a></p>
+          </div>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     );
   }
 }
