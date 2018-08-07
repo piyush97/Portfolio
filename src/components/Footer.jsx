e8b2c4696ce48da3fb3217695adf4b9359437320
Piyush Mehta <me@piyushmehta.com>
2018-08-07 19:29:43 +0530

    skillup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,9 @@ class Footer extends React.Component {
   render() {
     return (
   <StickyFooter bottomThreshold={50} normalStyles={{ backgroundColor: "#999999", padding: "2rem" }} stickyStyles={{ backgroundColor: "rgba(255,255,255,.8)", padding: "2rem" }} >
-    <center><Typography>Made With <FavoriteIcon style={{ color: "red"}}/> By </Typography><Avatar /> <b>Piyush Mehta</b></center>
+    <center><Typography>Made With <FavoriteIcon style={{ color: "red"}}/> By </Typography><Avatar /> <b>Piyush Mehta</b>
+        <Typography variant="display1"><a href="mailto:me@piyushmehta.com?Subject=Hello%20Piyush">Hire Me?</a></Typography>
+    </center>
   </StickyFooter>
     );
   }
